58b91e3455575bf275e68d4eaf3507c056f6bcbb
Daybson Paisante <daybson.paisante@outlook.com>
2016-08-29 11:11:56 -0300

    [exercise 04] * Gamepad.cs

--- sfml demos/C#/exercise 04/sfml.net/src/Gamepad.cs
+++ sfml demos/C#/exercise 04/sfml.net/src/Gamepad.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using static SFML.Window.Joystick;
 
 namespace sfml.net.src
 {
@@ -12,39 +11,11 @@ namespace sfml.net.src
         #region Fields
 
         private uint index;
-        public uint Index { get { return index; } }
-
         private bool isActive = false;
         private bool hasZ = false;
         private bool move = false;
         private uint buttonCount = 0;
         private int deadzone = 15;
-
-        public float ButtonCount
-        {
-            get { return Joystick.GetButtonCount(this.index); }
-        }
-        public Identification Identification
-        {
-            get { return Joystick.GetIdentification(this.index); }
-        }
-        public bool IsConnected
-        {
-            get { return Joystick.IsConnected(this.index); }
-        }
-
-
-        public uint GetButtonPressed
-        {
-            get
-            {
-                for (uint i = 0; i < this.buttonCount; i++)
-                    if (Joystick.IsButtonPressed(this.index, i))
-                        return i;
-                return
-                    this.buttonCount + 1;
-            }
-        }
 
         #endregion
 
@@ -54,7 +25,7 @@ namespace sfml.net.src
         public Gamepad(uint index)
         {
             this.index = index;
-            if (Joystick.IsConnected(this.index))
+            if(Joystick.IsConnected(this.index))
             {
                 Console.WriteLine(String.Format("Gamepad Connected\nID:{0}\n", Joystick.GetIdentification(this.index)));
                 this.hasZ = Joystick.HasAxis(this.index, Joystick.Axis.Z);
@@ -68,22 +39,13 @@ namespace sfml.net.src
             }
         }
 
-        public float GetAxisPosition(Axis axis)
+        public uint GetButtonPressed()
         {
-            return Joystick.GetAxisPosition(this.index, axis);
+            for (uint i = 0; i < this.buttonCount; i++)
+                if (Joystick.IsButtonPressed(this.index, i))
+                    return i;
+            return 0;
         }
-
-        public bool HasAxis(Axis axis)
-        {
-            return Joystick.HasAxis(this.index, axis);
-        }
-
-        public bool IsButtonPressed(uint button)
-        {
-            return Joystick.IsButtonPressed(this.index, button);
-        }
-
-        
 
         #endregion
     }
